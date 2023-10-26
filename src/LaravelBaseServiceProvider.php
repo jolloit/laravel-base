56aa5c25e5ec692e7a9649fc97d50fd0561c3870
jolloit <admin@jollo.it>
2023-10-26 21:13:17 +0000

    composer.json

---
+++ src/LaravelBaseServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jolloit\LaravelBase;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+}
